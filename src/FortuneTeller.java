d85e4267645d0dda1f2b8f5ea4c1f2ede56a43b9
Ellie Barker <ellen.m.barker20@gmail.com>
2018-11-09 08:38:14 -0600

    More Programs

---
+++ src/FortuneTeller.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class FortuneTeller {
+
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double weight = -999.0;
+		int age = -999;
+		int luckyNumber = -999;
+		Scanner input =new Scanner(System.in);
+		System.out.println("How much do you weigh?");
+		weight= input.nextDouble();
+		System.out.println("How old are you?");
+		age = input.nextInt();
+		System.out.print("What is your lucky number?");
+		luckyNumber =input.nextInt();
+		
+		
+		System.out.println("I predict that...");
+		System.out.println("You will die in hospital room number " + age + weight);
+		System.out.println("and " + luckyNumber + " people will come to your funeral.");
+		
+	 input.close();
+
+	}
+
+}
