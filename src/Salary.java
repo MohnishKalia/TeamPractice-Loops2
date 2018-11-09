7775526b1d46d1b144bf194a20c0f6ac293737af
Ellie Barker <ellen.m.barker20@gmail.com>
2018-11-09 08:39:37 -0600

    something stupid 2.0

--- src/Salary.java
+++ src/Salary.java
@@ -1,4 +1,31 @@
-
+import java.util.Scanner;
 public class Salary {
+	//barker
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	Scanner input= new Scanner(System.in);
 
+	int salary=0;
+	System.out.println("What is your salary?");
+	salary= input.nextInt();
+
+
+	if (salary<20000)
+	{
+		System.out.println("You must be a worker.");
+	}
+	else
+	{
+		if (salary<=40000)	
+		{
+			System.out.println("You must be a manager.");
+		}
+		if(salary<100000)
+			System.out.println("You must be a CEO.");
+	}
+		if(salary>=100000)
+			System.out.println("You must be the owner.");
+	}
+
+	}
 }
