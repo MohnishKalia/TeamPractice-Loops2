26a9c1cbfd4fbff506a28ca2ffd2818b83fc8348
Ellie Barker <ellen.m.barker20@gmail.com>
2018-11-12 08:06:02 -0600

    stuff

---
+++ src/BarkerOtherProgram.java
@@ -0,0 +1,16 @@
+
+public class BarkerOtherProgram {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int count=0;
+		
+		while(count<=100000)
+		{
+			System.out.println(count);
+			count= count+2; //or count+=2;
+		}
+		
+	}
+
+}
